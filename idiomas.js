0574ec9fe9d16566a27a76493cafadc3a57bf1f3
antoniomancera <antoniomanuelmancerahevilla@gmail.com>
2020-12-19 19:45:16 +0100

    no message

--- idiomas.js
+++ idiomas.js
@@ -1,1 +1,24 @@
+var time = new Date();
+    console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+	console.log("funciona java");
 
+/*
+initTranslation('index');
+
+
+
+var localization = {
+
+            title: "MuroDev: Español",
+
+            description: "Un sencillo sistema de traduccion por JavaScript.",
+
+            translate: "Translate to English"
+
+};
+
+
+
+
+var localization = {
+*/
